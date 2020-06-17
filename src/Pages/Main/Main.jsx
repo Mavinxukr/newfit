7e9a0cb279846763584ec90e8feb5f47b23065a0
SamborVadym <samborvadym@gmail.com>
2020-06-17 15:09:38 +0300

    main page

--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -1,30 +1,20 @@
 import React from 'react';
 // import styles from './Main.module.scss';
-import Header from '../../Components/Header/Header';
-import Sidebar from '../../Components/Sidebar/Sidebar';
-import Finance from '../../Components/Finance/Layout/Layout';
-// import EntryForm from '../../Components/Main/EntryForm/EntryForm';
+import EntryForm from '../../Components/Main/EntryForm/EntryForm';
 // import Login from '../../Components/Main/Login/Login';
 // import SingUp from '../../Components/Main/SingUp/SingUp';
-// import withPopup from '../../HOC/withPopup';
+import withPopup from '../../HOC/withPopup';
 
-const Main = () => (
-  <>
-    <Header />
-    <div style={{ display: 'flex' }}>
-      <Sidebar />
-      <Finance />
-    </div>
-  </>
-  // <button
-  //   type="button"
-  //   onClick={() => {
-  //     openPopup({
-  //       PopupContentComponent: EntryForm,
-  //     });
-  //   }}
-  // >open popup
-  // </button>
+const Main = ({ openPopup }) => (
+  <button
+    type="button"
+    onClick={() => {
+      openPopup({
+        PopupContentComponent: EntryForm,
+      });
+    }}
+  >open popup
+  </button>
 );
 
-export default Main;
+export default withPopup(Main);

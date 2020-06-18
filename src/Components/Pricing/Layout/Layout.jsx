1a70a04f3f7aad445ec0f09527d4b756b6cf3624
SamborVadym <samborvadym@gmail.com>
2020-06-18 14:51:55 +0300

    added footer and new icon

--- src/Components/Pricing/Layout/Layout.jsx
+++ src/Components/Pricing/Layout/Layout.jsx
@@ -1,35 +1,46 @@
 import React from 'react';
 import cx from 'classnames';
+import { Link } from 'react-router-dom';
+import Button from '../../../UI-Kit/Button/Button';
 import IconLogo from '../../../static/svg/EF.svg';
 import IconFacebook from '../../../static/svg/facebook1.svg';
 import IconTelegram from '../../../static/svg/telegram1.svg';
 import IconEmail from '../../../static/svg/email1.svg';
+import IconFooterArrow from '../../../static/svg/footerArrow.svg';
 import styles from './Layout.scss';
 
 const Layout = () => (
   <>
     <header className={styles.header}>
-      <a href="/">
+      <Link to="/">
         <IconLogo />
         <span className={styles.logoText}>Тренер</span>
-      </a>
+      </Link>
       <div>
         <a className={styles.headerLink} href="/">Подписки</a>
         <a className={cx(styles.headerLink, styles.opacityText)} href="/">Возможности</a>
       </div>
       <div className={styles.headerSocial}>
         <p className={styles.socialText}>Онлайн-чат с нами</p>
-        <a href="/" className={styles.socialLink}>
+        <Link to="/" className={styles.socialLink}>
           <IconFacebook />
-        </a>
-        <a href="/" className={styles.socialLink}>
+        </Link>
+        <Link to="/" className={styles.socialLink}>
           <IconTelegram />
-        </a>
-        <a href="/" className={styles.socialLink}>
+        </Link>
+        <Link to="/" className={styles.socialLink}>
           <IconEmail />
-        </a>
+        </Link>
       </div>
     </header>
+    <main className={styles.content}>
+      da
+    </main>
+    <footer className={styles.footer}>
+      <Button type="button" classNameWrapper={styles.footerButton}>
+        Связать с менеджером <IconFooterArrow className={styles.footerIcon} />
+      </Button>
+    </footer>
   </>
 );
 
